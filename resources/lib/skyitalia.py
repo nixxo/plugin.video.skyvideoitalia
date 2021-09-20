386a532aa8d55a84ae966f3fd9edd82fc323ca06
nixxo <nixxo@protonmail.com>
2021-09-20 18:13:29 +0200

    v0.0.4

--- resources/lib/skyitalia.py
+++ resources/lib/skyitalia.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json, re
 import urllib.request as urllib2
+from phate89lib import kodiutils
 
 
 class SkyItalia:
@@ -11,22 +12,26 @@ class SkyItalia:
     GET_VIDEO_DATA = 'https://apid.sky.it/vdp/v1/getVideoData?token={token}&caller=sky&rendition=web&id={id}'  # noqa: E501
     GET_VOD_ACCESS_TOKEN = 'https://apid.sky.it/vdp/v1/getVodAccessToken?token={token}&url={url}&dec=0'  # noqa: E501
     TOKEN = 'F96WlOd8yoFmLQgiqv6fNQRvHZcsWk5jDaYnDvhbiJk'
+    LOGOSDIR = ''
+    FANART = ''
 
     def __init__(self):
         opener = urllib2.build_opener()
         urllib2.install_opener(opener)
+        self.LOGOSDIR = '%sresources\\logos\\' % kodiutils.PATH_T
+        self.FANART = '%sresources\\fanart.png' % kodiutils.PATH_T
 
     def clean_title(self, title):
         import html
         title = html.unescape(title)
-        title = re.sub('^VIDEO:* ', '', title)
+        title = re.sub(r'^VIDEO:*\s+', '', title)
         return title
 
     def get_main(self):
         try:
             page = urllib2.urlopen(self.HOME).read().decode('utf-8')
         except :
-            self.log('get_main error: "%s" not available' % self.HOME)
+            kodiutils.log('get_main error: "%s" not available' % self.HOME)
             return
         m = re.search('"content":([\\s\\S]+?),\\s*"highlights"', page, re.S)
         if not m:
@@ -34,15 +39,23 @@ class SkyItalia:
             return
         try:
             menu = json.loads(m.group(1))
-            clean_menu = []
-            # remove deactivated menu items
-            for item in menu:
-                if menu[item]['active'] == 'Y':
-                    clean_menu.append(menu[item])
-        except :
-            self.log('get_main error: JSON decode error')
+        except:
+            kodiutils.log('get_main error: JSON decode error')
             return
-        return clean_menu
+        for item in menu:
+            # yield only active menu element
+            if menu[item]['active'] == 'Y':
+                section = item.strip('/')
+                yield {
+                    'label': menu[item]['label'],
+                    'params': {
+                        'section': section
+                    },
+                    'arts': {
+                        'thumb': '%s%s.png' % (self.LOGOSDIR, section),
+                        'fanart': self.FANART,
+                    },
+                }
     
     def get_section(self, section):
         try:
@@ -52,31 +65,67 @@ class SkyItalia:
             self.log('get_section error with section: "%s"' % section)
             return
 
-        section = []
         for match in re.finditer(
                 'menu-entry-sub[^"]*"><a href="%s/(.+?)">(.+?)</a>' % url,
                 page, re.S):
-            section.append({
-                'link': match[1],
-                'label': match[2],
-                })
-        return section
+            label = self.clean_title(match[2])
+            yield {
+                'label': label,
+                'params': {
+                    'section': section,
+                    'subsection': match[1],
+                    'title': label,
+                },
+                'arts': {
+                    'thumb': '%s%s\\%s.png' % (
+                        self.LOGOSDIR, section, match[1]),
+                    'fanart': self.FANART,
+                },
+            }
 
-    def get_subsection(self, section, subsection, page=0):
+    def get_subsection(self, section, subsection, title, page=0):
         url = self.GET_VIDEO_SEARCH
         url = url.replace('{token}', self.TOKEN)
         url = url.replace('{section}', section)
         url = url.replace('{subsection}', subsection)
         url = url.replace('{page}', str(page))
         try:
-            subsection = json.loads(urllib2.urlopen(url).read().decode('utf-8'))
+            subs = json.loads(urllib2.urlopen(url).read().decode('utf-8'))
         except :
             self.log('get_subsection error with sec/subsec: "%s/%s"' % [section, subsection])
             return
-        return subsection['assets']
+
+        yield {
+            'label': kodiutils.LANGUAGE(32001) % title,
+            'params': {
+                'section': section,
+                'subsection': subsection,
+                'playlist': title,
+            },
+            'arts': {
+                'thumb': '%s%s\\%s.png' % (
+                    self.LOGOSDIR, section, subsection),
+                'fanart': self.FANART,
+            },
+        }
+
+        for item in subs['assets']:
+            yield {
+                'label': self.clean_title(item['title']),
+                'params': {
+                    'asset_id': item['asset_id']
+                },
+                'arts': {
+                    'thumb': item['video_still'],
+                    'fanart': self.FANART,
+                },
+                'videoInfo': {
+                    'mediatype': 'video',
+                },
+                'isPlayable': True,
+            }
 
     def get_playlist(self, section, subsection):
-        # web_pdb.set_trace()
         url = self.GET_PLAYLISTS
         url = url.replace('{token}', self.TOKEN)
         url = url.replace('{section}', section)
@@ -86,7 +135,18 @@ class SkyItalia:
         except :
             self.log('get_playlist error with sec/subsec: "%s/%s"' % [section, subsection])
             return
-        return playlist
+
+        for item in playlist:
+            yield {
+                'label': self.clean_title(item['title']),
+                'params': {
+                    'playlist_id': item['playlist_id'],
+                },
+                'arts': {
+                    'thumb': item['thumb'],
+                    'fanart': self.FANART,
+                },
+            }
 
     def get_playlist_content(self, playlist_id):
         url = self.GET_PLAYLIST_VIDEO
@@ -97,7 +157,21 @@ class SkyItalia:
         except :
             self.log('get_playlist_content with playlist_id: "%s"' % playlist_id)
             return
-        return playlist['assets']
+        for item in playlist['assets']:
+            yield {
+                'label': self.clean_title(item['title']),
+                'params': {
+                    'asset_id': item['asset_id']
+                },
+                'arts': {
+                    'thumb': item['video_still'],
+                    'fanart': self.FANART,
+                },
+                'videoInfo': {
+                    'mediatype': 'video',
+                },
+                'isPlayable': True,
+            }
 
     def get_access_token_url(self, url, token):
         url = self.GET_VOD_ACCESS_TOKEN
--- resources/main.py
+++ resources/main.py
@@ -7,16 +7,16 @@ class SkyVideoItalia(object):
 
     def __init__(self):
         self.skyit = SkyItalia()
-        self.skyit.log = kodiutils.log
-        self.logosdir = '%sresources\\logos\\' % kodiutils.PATH
 
-    def addPlayItems(self, items):
+    def addItems(self, items):
         for item in items:
             kodiutils.addListItem(
-                self.skyit.clean_title(item['title']),
-                {'asset_id': item['asset_id']},
-                videoInfo={'mediatype': 'video'},
-                arts={'thumb': item['video_still']}, isFolder=False)        
+                label=item.get('label'),
+                params=item.get('params'),
+                videoInfo=item.get('videoInfo'),
+                arts=item.get('arts'),
+                isFolder=False if item.get('isPlayable') else True,
+            )
 
     def main(self):
         params = staticutils.getParams()
@@ -32,53 +32,28 @@ class SkyVideoItalia(object):
         elif 'playlist_id' in params:
             # PLAYLIST CONTENT
             playlist_content = self.skyit.get_playlist_content(params['playlist_id'])
-            self.addPlayItems(playlist_content)
+            self.addItems(playlist_content)
 
         elif all(x in params for x in ['playlist', 'section', 'subsection']):
             # PLAYLIST SECTION
             playlist = self.skyit.get_playlist(
                 params['section'], params['subsection'])
-            for item in playlist:
-                kodiutils.addListItem(
-                    self.skyit.clean_title(item['title']),
-                    {'playlist_id': item['playlist_id']},
-                    arts={'thumb': item['thumb']}, isFolder=True)
+            self.addItems(playlist)
 
         elif all(x in params for x in ['title', 'section', 'subsection']):
             # SUBSECTION MENU
             subsection_content = self.skyit.get_subsection(
-                params['section'], params['subsection'])
-            thumb = '%s%s\\%s.png' % (
-                self.logosdir, params['section'], params['subsection'])
-            kodiutils.addListItem(
-                'Playlist di %s' % params['title'],
-                {'section': params['section'],
-                 'subsection': params['subsection'],
-                 'playlist': params['title']},
-                arts={"thumb": thumb}, isFolder=True)
-            self.addPlayItems(subsection_content)
+                params['section'], params['subsection'], params['title'])
+            self.addItems(subsection_content)
 
         elif 'section' in params:
             # SECTION MENU
             section_content = self.skyit.get_section(params['section'])
-            for item in section_content:
-                title = self.skyit.clean_title(item['label'])
-                thumb = '%s%s\\%s.png' % (
-                    self.logosdir, params['section'], item['link'])
-                kodiutils.addListItem(
-                    self.skyit.clean_title(item['label']),
-                    {'section': params['section'],
-                     'subsection': item['link'],
-                     'title': title},
-                    arts={'thumb': thumb}, isFolder=True)
+            self.addItems(section_content)
 
         else:
             # MAIN MENU
             menu = self.skyit.get_main()
-            for item in menu:
-                section = item['link'].strip('/')
-                thumb = '%s%s.png' % (self.logosdir, section)
-                kodiutils.addListItem(
-                    item['label'], {'section': section},
-                    arts={'thumb': thumb}, isFolder=True)
+            self.addItems(menu)
+
         kodiutils.endScript()
